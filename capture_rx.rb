01931a3290c1ec3082980a3ade0bd13307c082e6
Hiroaki Akanuma <hiroaki.akanuma@gmail.com>
2016-12-25 20:30:26 +0900

    RxRuby使用版

---
+++ capture_rx.rb
@@ -0,0 +1,71 @@
+require 'packetfu'
+require 'rx'
+require './ouis.rb'
+include PacketFu
+
+class Rx::BehaviorSubject
+  public :check_unsubscribed
+end
+
+def get_capture(iface)
+  subject = Rx::BehaviorSubject.new('')
+  source = subject.as_observable.select {|pkt| dash_packet?(pkt) }
+
+  source.subscribe(
+    lambda do |pkt|
+      time = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
+
+      if UDPPacket.can_parse?(pkt)
+        packet = UDPPacket.parse(pkt)
+        src_ip = IPHeader.octet_array(packet.ip_src).join('.')
+        dst_ip = IPHeader.octet_array(packet.ip_dst).join('.')
+        protocol = 'udp'
+      elsif ARPPacket.can_parse?(pkt)
+        packet = ARPPacket.parse(pkt)
+        src_ip = packet.arp_saddr_ip
+        dst_ip = packet.arp_daddr_ip
+        protocol = 'arp'
+      else
+        next
+      end
+
+      src_mac, dst_mac, vendor_name = get_common_values(packet)
+      output(time, src_mac, dst_mac, src_ip, dst_ip, protocol, vendor_name)
+    end,
+    lambda {|err| puts "Error: #{err}" },
+    lambda { puts 'Completed.' }
+  )
+
+  cap = Capture.new(iface: iface, start: true)
+  cap.stream.each do |pkt|
+    subject.on_next pkt
+  end
+end
+
+def dash_packet?(pkt)
+  return false unless EthPacket.can_parse?(pkt)
+  get_vendor_name(EthHeader.str2mac(EthPacket.parse(pkt).eth_src)).downcase.include?('amazon')
+end
+
+def output(time, src_mac, dst_mac, src_ip, dst_ip, protocol, vendor_name)
+  puts "time:#{time}, src_mac:#{src_mac}, dst_mac:#{dst_mac}, src_io:#{src_ip}, dst_ip:#{dst_ip}, protocol:#{protocol}, vendor:#{vendor_name}"
+end
+
+def get_common_values(packet)
+  src_mac = EthHeader.str2mac(packet.eth_src)
+  dst_mac = EthHeader.str2mac(packet.eth_dst)
+  vendor_name = get_vendor_name(src_mac)
+  return src_mac, dst_mac, vendor_name
+end
+
+def get_vendor_name(mac)
+  return '' if mac.nil?
+  oui = mac.split(':').slice(0, 3).join('-')
+  OUIS[oui.upcase]
+end
+
+if $0 == __FILE__
+  iface = ARGV[0]
+  puts "Capturing for interface: #{iface}"
+  get_capture(iface)
+end
---
+++ post_to_slack_rx.rb
@@ -0,0 +1,62 @@
+require 'packetfu'
+require 'open3'
+require 'json'
+require 'rx'
+include PacketFu
+
+FILTER = nil
+MAC_OF_DASH = 'Dash Button の MACアドレス'
+SLACK_API_URL = 'Slack Incoming Webhook URL'
+INTERVAL_SECONDS = 2
+
+class Rx::BehaviorSubject
+  public :check_unsubscribed
+end
+
+def get_capture(iface)
+  subject = Rx::BehaviorSubject.new('')
+  source = subject.as_observable.select {|pkt| target_dash_pushed?(pkt) }.debounce(INTERVAL_SECONDS)
+
+  source.subscribe(
+    lambda do |pkt|
+      post_to_slack
+
+      t_stamp = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
+      puts "#{t_stamp} Posted to Slack."
+    end,
+    lambda {|err| puts "Error: #{err}" },
+    lambda { puts 'Completed.' }
+  )
+
+  cap = Capture.new(iface: iface, filter: FILTER, start: true)
+  cap.stream.each do |pkt|
+    subject.on_next pkt
+  end
+end
+
+def target_dash_pushed?(pkt)
+  return false unless EthPacket.can_parse?(pkt)
+  EthHeader.str2mac(EthPacket.parse(pkt).eth_src) == MAC_OF_DASH
+end
+
+def post_to_slack
+  api_url = SLACK_API_URL
+  payload = {
+    channel:    '#akanuma_private',
+    username:   'dash',
+    icon_emoji: ':squirrel:',
+    text:       'Hello World from Dash Button!!'
+  }
+  command = "curl -X POST --data-urlencode 'payload=#{payload.to_json}' #{api_url}"
+  puts command
+  output, std_error, status = Open3.capture3(command)
+  puts output
+  puts std_error
+  puts status
+end
+
+if $0 == __FILE__
+  iface = ARGV[0]
+  puts "Capturing for interface: #{iface}"
+  get_capture(iface)
+end
